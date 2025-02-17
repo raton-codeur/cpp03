4225bc5da907fddc4911f4092fbb1d885a52c656
Quentin Hauuy <quentinhauuy@hotmail.com>
2025-02-17 18:08:34 +0100

    blabla

--- repo/ex00/main.cpp
+++ repo/ex00/main.cpp
@@ -2,11 +2,47 @@
 
 int main()
 {
-	{ClapTrap	c;}
-	ClapTrap	c("bob");
+	{
+		std::cout << "--- basic tests ---" << std::endl;
+		ClapTrap ct1;
+		ClapTrap ct2("bob");
+		ClapTrap ct3(ct2);
+		ClapTrap ct4;
+		ct4 = ct3;
+		ct4.takeDamage(1);
+	}
+	
+	std::cout << "--- attack tests ---" << std::endl;
+	{
+		ClapTrap c("louis");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+	}
+	{
+		ClapTrap c("marc");
+		c.takeDamage(100);
+		c.attack("hugo");
+	}
 
-	c.attack("john");
-	c.takeDamage(15);
-	c.takeDamage(1);
-	c.attack("john");
+	std::cout << "--- takeDamage tests ---" << std::endl;
+	{
+		ClapTrap c("jack");
+		c.takeDamage(3);
+		c.takeDamage(2);
+		c.takeDamage(6);
+		c.takeDamage(1);
+	}
+	{
+		ClapTrap c("michael");
+		c.takeDamage(10);
+	}
 }
