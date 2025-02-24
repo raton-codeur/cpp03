cc6407f35b26ad8c297e1428fd5f6a68863f834a
qhauuy <qhauuy@student.42mulhouse.fr>
2025-02-24 12:28:05 +0100

    blabla

---
+++ repo/ex00/main.cpp
@@ -0,0 +1,38 @@
+#include "ClapTrap.hpp"
+
+int main()
+{
+	std::cout << "--- tests 1 ---" << std::endl;
+	{
+		ClapTrap ct1;
+		ClapTrap ct2("bob");
+		ClapTrap ct3(ct2);
+		ClapTrap ct4;
+		ct4 = ct3;
+	}
+	std::cout << "--- tests 2 ---" << std::endl;
+	{
+		ClapTrap c("louis");
+		c.attack("john");
+		c.beRepaired(3);
+		c.attack("john");
+		c.beRepaired(5);
+		c.beRepaired(6);
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("john");
+		c.attack("david");
+		c.beRepaired(8);
+		c.takeDamage(6);
+	}
+	std::cout << "--- tests 3 ---" << std::endl;
+	{
+		ClapTrap c("marc");
+		c.takeDamage(100);
+		c.takeDamage(4);
+		c.attack("hugo");
+		c.beRepaired(3);
+	}
+}
